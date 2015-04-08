cc5981872e2fec8d7b4250040374d7739485dc3a
Danish Iqbal <iqbald@flip2.engr.oregonstate.edu>
2015-04-08 16:45:21 -0700

    test2

--- projects/iqbald/dominion/dominion.c
+++ projects/iqbald/dominion/dominion.c
@@ -670,6 +670,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       while(drawntreasure<2){
 	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
 	  shuffle(currentPlayer, state);
+	  drawtreasure++;
 	}
 	drawCard(currentPlayer, state);
 	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
