578d4968cbadfefade05cd1087401860c7aa61d0
Miles Curry <currymi@onid.orst.edu>
2015-04-11 13:09:12 -0700

    remodel refactor complete

--- projects/currymi/dominion/dominion.c
+++ projects/currymi/dominion/dominion.c
@@ -805,7 +805,33 @@ int mineEffect(int currentPlayer, int choice1, int choice2, int handPos, struct 
 			
     return 0;
 }
+int remodelEffect(int currentPlayer, int choice1, int choice2, int handPos, struct gameState *state){
+    int j;
+    int i;
 
+      j = state->hand[currentPlayer][choice1];  //store card we will trash
+
+      if ( (getCost(state->hand[currentPlayer][choice1]) + 2) > getCost(choice2) )
+	{
+	  return -1;
+	}
+
+      gainCard(choice2, state, 0, currentPlayer);
+
+      //discard card from hand
+      discardCard(handPos, currentPlayer, state, 0);
+
+      //discard trashed card
+      for (i = 0; i < state->handCount[currentPlayer]; i++)
+	{
+	  if (state->hand[currentPlayer][i] == j)
+	    {
+	      discardCard(i, currentPlayer, state, 0);			
+	      break;
+	    }
+	}
+    return 0;
+}
 
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
@@ -844,30 +870,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return mineEffect(currentPlayer, choice1, choice2, handPos, state);
 			
     case remodel:
-      j = state->hand[currentPlayer][choice1];  //store card we will trash
-
-      if ( (getCost(state->hand[currentPlayer][choice1]) + 2) > getCost(choice2) )
-	{
-	  return -1;
-	}
-
-      gainCard(choice2, state, 0, currentPlayer);
-
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-
-      //discard trashed card
-      for (i = 0; i < state->handCount[currentPlayer]; i++)
-	{
-	  if (state->hand[currentPlayer][i] == j)
-	    {
-	      discardCard(i, currentPlayer, state, 0);			
-	      break;
-	    }
-	}
-
-
-      return 0;
+      return  remodelEffect(currentPlayer, choice1, choice2, handPos, state);
 		
     case smithy:
       smithyEffect(currentPlayer, handPos, state);
