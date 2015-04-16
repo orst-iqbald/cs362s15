d04b81575b143edaaf040f5fc265ab59e4f3a799
kevin <parkske@onid.oregonstate.edu>
2015-04-15 18:23:33 -0700

    create unittest1/2, update Makefile

---
+++ projects/parkske/dominion/unittest2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+
+#define NUM_PLAYERS 2
+//#define MYDEBUG
+
+int main(int argc, char* argv[])
+{
+    printf("\n\n****BEGIN UNITTEST2: GainCard****\n");
+    printf("****END UNITTEST 2****\n");
+    return 0;
+}    
