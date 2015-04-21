9d1ec9ba7bf7d85052ade73180ddbd8ef8c1f14a
Bobby Hines <hinesro@onid.oregonstate.edu>
2015-04-21 15:55:31 -0700

    Add assignment 3 files

---
+++ projects/hinesro/dominion/cardtest4.c
@@ -0,0 +1,6 @@
+/*
+(20 points) Write unit tests for four Dominion cards implemented in 
+dominion.c.  These tests should be checked in as cardtest1.c, cardtest2.c,
+cardtest3.c, and cardtest4.c. (For example, create a test for smithy 
+card.). It is mandatory to test smithy and adventurer card. 
+*/
---
+++ projects/hinesro/dominion/unittest1.c
@@ -0,0 +1,6 @@
+/*
+(20 points) Write unit tests for four functions (not card implementations 
+or cardEffect) in dominion.c. Check these tests in as unittest1.c,
+unittest2.c, unittest3.c, and unittest4.c. (for example, you can create a 
+test for updateCoins() method)
+*/
