31df8e0de3f5122e79ad251e173d842a5aae4dd4
kevin <parkske@onid.oregonstate.edu>
2015-04-21 20:32:02 -0700

    some basic testing unittest3.c

--- projects/parkske/dominion/unittest3.c
+++ projects/parkske/dominion/unittest3.c
@@ -7,6 +7,11 @@
 #define TOFLAGMAX 1
 #define MYDEBUG
 
+int testDiscardCard(int m_handPos, int player, struct gameState *state, int flag)
+{
+	return discardCard(m_handPos, player, state, flag);
+}
+
 int main(int argc, char* argv[])
 {
     printf("\n\n****BEGIN UNITTEST3: DiscardCard****\n");
@@ -14,8 +19,9 @@ int main(int argc, char* argv[])
     int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, 
            sea_hag, tribute, smithy};
     int i;
+	int j;
     int players[NUM_PLAYERS];
-	int playerCardCount[NUM_PLAYERS];
+	int playerHandCount[NUM_PLAYERS];
     int flag = 0;
 	enum CARD m_supplyPos;
 	
@@ -23,27 +29,61 @@ int main(int argc, char* argv[])
     initializeGame(NUM_PLAYERS, k, 5, m_state);
     printf("PASS\n");
 	
+	//create num of players and record their hand count
 	for(i=0; i < NUM_PLAYERS; i++)
     {
         players[i] = i;
-		playerCardCount[i] = m_state->handCount[i];
+		playerHandCount[i] = m_state->handCount[i];
     }
-    for(m_supplyPos=0; m_supplyPos <= treasure_map; m_supplyPos++)
-    {
-        for(i=0; i < NUM_PLAYERS; i++)
-        {
-            for(flag=0; flag < TOFLAGMAX; flag++)
-            {
-                int pass = 0;
-                if(flag == 0)
-                {
-                    if(testDiscardCard(m_handPos, players[i], m_state, flag) == 0)
-                }
-                printf("Player%d testDiscardCard flag:%d card:%d.....", i+1, flag, m_supplyPos);
-                
-            }
-        }
-    }
+	
+	for(i=0; i < NUM_PLAYERS; i++)
+	{
+		for(j=playerHandCount[i] - 1; j >= 0; j--)
+		{
+			int m_handCount = m_state->handCount[i];
+			int m_handPos = m_handCount - 1;
+			int m_playedCount = m_state->playedCardCount;
+			int pass = 0;
+			if(m_handCount % 2 == 0)
+			{
+				flag = 0;
+			}
+			else
+				flag = 1;
+			printf("P%d testDiscardCard ", i+1);
+			printf("flag:%d ", flag);
+			printf("discard %d.....", m_handCount);
+			if(testDiscardCard(m_handPos, players[i], m_state, flag) == 0)
+			{
+				if(flag == 0)
+				{
+					if(m_state->playedCardCount != m_playedCount + 1)
+					{
+						printf("FAIL - playedCardCount; ");
+						pass = 1;
+					}
+					if(m_state->playedCards[m_state->playedCardCount + 1] != m_state->hand[i][m_handPos])
+					{
+						printf("FAIL - playedCards; ");
+						pass = 1;
+					}
+				}
+				if(m_handCount - 1 != m_state->handCount[i])
+				{
+					printf("FAIL - handCount; ");
+					pass = 1;
+				}
+				if(pass != 0)
+				{
+					printf("\n");
+				}
+				else
+				{
+					printf("PASS\n");
+				}
+			}
+		}
+	}
     printf("****END UNITTEST 2****\n");
     return 0;
 }    
