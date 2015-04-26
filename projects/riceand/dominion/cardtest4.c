e3e099b3161dc6b2c18833af585b04704fee3356
Andrew Rice <ajjrice@gmail.com>
2015-04-25 23:26:52 -0400

    Add cardtest1 - cardtest4 .c files

---
+++ projects/riceand/dominion/cardtest4.c
@@ -0,0 +1,45 @@
+/* -------------------------------------------
+ * Unit Tests to cover the Council Room card 
+---------------------------------------------*/
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+
+//set to 0 to turn OFF extra print statements
+#define EXTRA_INFO 1
+
+void testCouncilRoom() {
+
+	//set up gamestate
+	int player = 0;
+  int p = 2;
+	int k[10] = {adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall};
+  	srand(time(0));
+  	int randSeed = (rand() % 10000) + 1;
+  	if (EXTRA_INFO == 1)
+  		printf("randSeed = %d\n", randSeed);
+  	
+  	struct gameState s;
+
+  	int r = initializeGame(p, k, randSeed, &s);
+  	if (EXTRA_INFO == 1)
+  		printf("r after init call = %d\n", r);
+
+  	assert(r == 0); //else rest will fail
+  	
+  	//end gamestate set up
+
+	/*
+	Council Room testing:
+		1. 
+	*/
+}
+
+ int main(int argc, char* argv[]) {
+
+ 	testCouncilRoom();
+
+ 	return 0;
+ }
