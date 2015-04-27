9d73e2dd879b0ed460f5266988af661a27c6f3a5
olsonso <olsonso@onid.oregonstate.edu>
2015-04-27 00:45:52 -0600

    Assignment 3

--- projects/olsonso/dominion/unittest3.c
+++ projects/olsonso/dominion/unittest3.c
@@ -11,6 +11,9 @@
 #define TEST 1
 #define CARDS 27
 
+// this test will first go and assign the correct values with each of the given cards. Once ran though
+//they should output the correct values.
+
 int cardCosts[CARDS] = {0, 2, 5, 8, 0, 3, 6, 6, 5, 4, 4, 5, 4, 4, 3, 4, 3, 5, 3, 5, 3, 4, 2, 5, 4, 4, 4};
 
 const char *cardNames[] = {"curse", "estate", "duchy", "province", "copper", "silver", "gold", "adventurer", "council_room",
@@ -18,26 +21,26 @@ const char *cardNames[] = {"curse", "estate", "duchy", "province", "copper", "si
     "ambassador", "cutpurse", "embargo", "outpost", "salvager", "sea_hag", "treasure_map"};
 
 int main() {
-    int i, c;
+    int i;
+    int cost;
     printf ("Now testing getCost():\n");
-    //loop to go through all the cards in the game. Added the +1 to test an input int input.
+
+
+    //start loop through cards
+    //should print out each card indiviually with their own value
+    //should be 27
     for (i = 0; i < CARDS + 1; ++i) {
 #if (TEST == 1)
-        printf("Test cost of %s card.\n", cardNames[i]);
+        printf("the given cost of %s card /n", cardNames[i]);
 #endif
-        c = getCost(i);
+        cost = getCost(i);
 #if (TEST == 1)
-        //case that cost of card returned from function matches value in array
-        if(c == cardCosts[i]) {
-            printf("Test Passed!\n");
+        //checks to see if the values match up
+        if(cost == cardCosts[i]) {
+            printf("Test Passed\n");
         }
-        //case that cardNumber input to function is not in the list
-        else if(c < 0) {
-            printf("Test Passed: \n", c);
-        }
-        //case that card cost doesn't match 
         else {
-            printf("Test Failed: ", cardCosts[i], c);
+            printf("Test Failed: ", cardCosts[i], cost);
         }
 #endif
     }
