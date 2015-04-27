68b3fd88ac4edc608c7afc7ddd50e274d300bcbf
ismouton <github@ismouton.org>
2015-04-27 15:22:05 +1200

    submit

---
+++ projects/moutoni/assignment_3/dominion/unittest1.c
@@ -0,0 +1,36 @@
+//int updateCoins(int player, struct gameState *state, int bonus)
+
+//struct gameState {
+//    int numPlayers; //number of players
+//    int supplyCount[treasure_map+1];  //this is the amount of a specific type of card given a specific number.
+//    int embargoTokens[treasure_map+1];
+//    int outpostPlayed;
+//    int outpostTurn;
+//    int whoseTurn;
+//    int phase;
+//    int numActions; /* Starts at 1 each turn */
+//    int coins; /* Use as you see fit! */
+//    int numBuys; /* Starts at 1 each turn */
+//    int hand[MAX_PLAYERS][MAX_HAND];
+//    int handCount[MAX_PLAYERS];
+//    int deck[MAX_PLAYERS][MAX_DECK];
+//    int deckCount[MAX_PLAYERS];
+//    int discard[MAX_PLAYERS][MAX_DECK];
+//    int discardCount[MAX_PLAYERS];
+//    int playedCards[MAX_DECK];
+//    int playedCardCount;
+//};
+
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "rngs.h"
+
+int main()
+{
+    
+}
