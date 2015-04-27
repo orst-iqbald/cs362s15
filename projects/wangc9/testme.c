16fe8752d650a8084b5d2dd0ca959b699bd9a300
Chunyu Wang <chunyuwang123@gmail.com>
2015-04-27 00:24:43 -0500

    HW3

---
+++ projects/wangc9/testme.c
@@ -0,0 +1,73 @@
+// Quiz 2
+// Chunyu Wang
+// gcc -Wall -std=c99 -o testme testme.c
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+	//ASCII: SPACE=32, ()=40 41, []=91 93, {}=123 125, a=97, x=120
+    char ch = rand()%(126-32)+32;	
+    return ch;
+}
+
+char *inputString()
+{
+    char *str = malloc(6 * sizeof(char));
+	str[5] = '\0';
+	char ch[4];
+	ch[0] = 'r';
+	ch[1] = 'e';
+	ch[2] = 's';
+	ch[3] = 't';	
+	int i;
+	for(i = 0; i < 5; i++) 
+	{
+		str[i] = ch[rand() % 4];
+	}
+    return str;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' ' && state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
