dd9778b8abe67f5feabec9481174dd9650b2d300
Justin Hammar <justin.hammar@gmail.com>
2015-05-06 21:02:02 -0500

    added weightedRand() to randomtestcard.c and added to randomhistory.txt

--- projects/hammarj/dominion/randomtestcard.c
+++ projects/hammarj/dominion/randomtestcard.c
@@ -13,6 +13,22 @@ int insertCardRand(struct gameState* gs, int card);
 //initialize every gameState variable to some random but valid number
 void genRandState(struct gameState* gs);
 
+//Returns a weighted random number based upon the given ranges and their 
+//corresponding weights.  Returns -1 on failure.
+//ranges[]: ranges of values. ex. [2, 5, 6] -> [2,5), [5,6), [6,RAND_MAX)
+//rangesLen: length of ranges[]
+//weights[]: weights of the various ranges specified by ranges[].  An 
+//individual weight divided by the sum of the weights gives the chance that a 
+//number from the corresponding range will be returned
+//weightsLen: length of weights[]
+//PRECONDITION: ranges[] must have increasing values. Every value must be //nonnegative. rangesLen and weightsLen must be equal. srand() must already be
+//set
+int weightedRand(int ranges[], int rangesLen, int weights[], int weightsLen);
+
+//prints some of the variables of th gamestate
+//debug purposes
+void printGameState(struct gameState* gs);
+
 //tests cardSmithy()
 int main() {
     int i;
@@ -35,29 +51,37 @@ int main() {
         //make a copy of the gameState
         memcpy(gs0, gs, sizeof(struct gameState));
         
+        //printGameState(gs0);
+        //printGameState(gs);
+        
         res = cardSmithy(gs, handpos);
         assert(res == 0);
+        
+        //printGameState(gs0);
+        //printGameState(gs);
                  
         if (gs->deckCount[player] != 0 || gs->discardCount[player] != 0) {
             //deck/discard should go down by 3 cards total
             assert( (gs->deckCount[player] + gs->discardCount[player]) - 
                     (gs0->deckCount[player] + gs0->discardCount[player]) == -3);
-            assert(gs->handCount[player] - gs0->handCount[player] == 3);
+            //+3 cards but a card also gets played
+            assert(gs->handCount[player] - gs0->handCount[player] == 2);
         } else {
             //that is, unless both deck and discard get depleted
             assert( (gs->deckCount[player] + gs->discardCount[player]) - 
                     (gs0->deckCount[player] + gs0->discardCount[player]) <= 0);
             assert( (gs->deckCount[player] + gs->discardCount[player]) - 
                     (gs0->deckCount[player] + gs0->discardCount[player]) >= -3);
-            assert(gs->handCount[player] - gs0->handCount[player] >= 0);
-            assert(gs->handCount[player] - gs0->handCount[player] <= 3);
+            assert(gs->handCount[player] - gs0->handCount[player] >= -1);
+            assert(gs->handCount[player] - gs0->handCount[player] <= 2);
         }
         if (gs->playedCardCount - gs0->playedCardCount != 1) {
-            printf("%i, ", i);
+            printf("%i*, ", i);
         }
     }
     
-    printf("\n%i test runs of randomtestcard.c executed\n\n", i);
+    printf("\n*Failed due to playedCardCount\n");
+    printf("%i test runs of randomtestcard.c executed\n\n", i);
     
     return 0;
 }
@@ -80,10 +104,11 @@ int insertCardRand(struct gameState* gs, int card) {
 
 void genRandState(struct gameState* gs) {
     int i, j;
+    ranges
     
-    gs->numPlayers = rand() % MAX_PLAYERS;
+    gs->numPlayers = (rand() % 3) + 2;
     for (i = 0; i < treasure_map+1; ++i) {
-        gs->supplyCount[i] = rand() % MAX_DECK;
+        gs->supplyCount[i] = rand() % 16;
     }
     for (i = 0; i < treasure_map+1; ++i) {
         //15 embargo tokens total
@@ -97,25 +122,79 @@ void genRandState(struct gameState* gs) {
     gs->coins = rand();
     gs->numBuys = rand();
     for (i = 0; i < gs->numPlayers; ++i) {
-        gs->handCount[i] = rand() % MAX_HAND;
+        gs->handCount[i] = 
+                weightedRand((int[3]){0,10,MAX_HAND}, 3, (int[3]){99,1,0}, 3);
         for (j = 0; j < gs->handCount[i]; ++j) {
             gs->hand[i][j] = rand() % treasure_map+1;
         }
     }
     for (i = 0; i < gs->numPlayers; ++i) {
-        gs->deckCount[i] = rand() % MAX_DECK;
+        gs->deckCount[i] = 
+                weightedRand((int[3]){0,40,MAX_DECK}, 3, (int[3]){95,5,0}, 3);
         for (j = 0; j < gs->deckCount[i]; ++j) {
             gs->deck[i][j] = rand() % treasure_map+1;
         }
     }
     for (i = 0; i < gs->numPlayers; ++i) {
-        gs->discardCount[i] = rand() % MAX_DECK;
+        gs->discardCount[i] = 
+                weightedRand((int[3]){0,40,MAX_DECK}, 3, (int[3]){95,5,0}, 3);
         for (j = 0; j < gs->discardCount[i]; ++j) {
             gs->discard[i][j] = rand() % treasure_map+1;
         }
     }
-    gs->playedCardCount = rand() % MAX_DECK;
+    gs->playedCardCount = rand() % 10;
     for (i = 0; i < gs->playedCardCount; ++i) {
         gs->playedCards[i] = rand() % treasure_map+1;
     }
+}
+
+int weightedRand(int ranges[], int rangesLen, int weights[], int weightsLen) {
+    int i;
+    int weightSum = 0;
+    int rawRange;
+    
+    //ensure that the arrays are the same length
+    if (rangesLen != weightsLen)
+        return -1;
+    
+    //sum weights[]
+    for (i = 0; i < weightsLen; ++i) {
+        weightSum += weights[i];
+    }
+    
+    //pick a range based on their weights
+    rawRange = rand() % weightSum;
+    for (i = 0; i < weightLen; ++i) {
+        rawRange -= weights[i];
+        if (rawRange < 0) {
+            break;
+        }
+    }
+    
+    //ensure that the range has only nonnegative values
+    if (ranges[i] < 0)
+        return -1;
+    
+    //ensure that the range is non-empty
+    if (ranges[i] >= ranges[i+1])
+        return -1;
+    
+    //calculate the chosen range and return a random number within that range
+    if (i + 1 < rangesLen) {
+        return ( rand() % (ranges[i+1] - ranges[i]) ) + ranges[i];
+    } else {
+        return ( rand() % (RAND_MAX - ranges[i]) ) + ranges[i];
+    }
+}
+
+void printGameState(struct gameState* gs) {
+    int p = gs->whoseTurn;
+    
+    printf("\n");
+    printf("numPlayers: %i\n", gs->numPlayers);
+    printf("whoseTurn: %i\n", gs->whoseTurn);
+    printf("handCount[p]: %i\n", gs->handCount[p]);
+    printf("deckCount[p]: %i\n", gs->deckCount[p]);
+    printf("discardCount[p]: %i\n", gs->discardCount[p]);
+    printf("playedCardCount: %i\n", gs->playedCardCount);
 }
