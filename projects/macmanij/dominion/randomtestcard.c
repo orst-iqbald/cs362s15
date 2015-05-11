0cc63a063fef3f225880e2d9d42062d107869eff
Jason MacManiman <macmanij@onid.oregonstate.edu>
2015-05-10 18:04:51 -0700

    random test card: smithy not implemented correctly still?

--- projects/macmanij/dominion/randomtestcard.c
+++ projects/macmanij/dominion/randomtestcard.c
@@ -23,6 +23,7 @@
 int main(int argc, char** argv) {
 	struct gameState g;
 	int i = 0;
+	int k = 0;
 	int preTreasure = 0;
 	int postTreasure = 0;
 	int preHand = 0;
@@ -36,7 +37,7 @@ int main(int argc, char** argv) {
 
 	int actioncards[10] = { adventurer, smithy, cutpurse, outpost, gardens, village, sea_hag, mine, minion, embargo };
 	
-	for(i = 0; i < 100; i++) {
+	for(k = 0; k < 100; k++) {
 		//for each iteration of the test, the number of treasure
 		//cards and cards in hand will be reset to 0, and the 
 		//gamestate initialized.
@@ -70,8 +71,35 @@ int main(int argc, char** argv) {
 	printf("Treasures PRE: %d\n", preTreasure);
 	printf("Cards PRE: %d\n", preHand);
 
-	
+	// play the smithy card and then check the number of treasure cards
+	// and cards-in-hand again, making sure they change as expected.
+	playSmithyCard(&g, currPlayer, handPos);
 
+	for(i = 0; i < g.handCount[currPlayer]; i++) {
+	if(g.hand[currPlayer][i] == copper ||
+		g.hand[currPlayer][i] == silver ||
+		g.hand[currPlayer][i] == gold)
+		{
+			postTreasure++;
+		} else {
+			handPos = i;
+		}
+	
+		postHand = i+1;
+	}
+
+	printf("Treasures POST: %d\n", postTreasure);
+	printf("Cards POST: %d\n", postHand);
+
+	if(preTreasure > postTreasure) {
+		printf("ERROR: preTreasures > postTreasures!\n");
+	}
+
+	if((preHand + 3) != postHand) {
+		printf("ERROR: preHand+3 != postHand!\n");
+	}
+
+	printf("______________________________________________\n\n");
 	}
 	
 	return 0;
