5eff8ce25fa3eaf925c8b4866111edbf06a818d3
garcikat <garcikat@onid.oregonstate.edu>
2015-05-10 20:20:03 -0700

    Update assignment4: randomtestcard.c

--- projects/garcikat/dominion/randomtestcard.c
+++ projects/garcikat/dominion/randomtestcard.c
@@ -14,36 +14,44 @@ int checkSmithy(int player, struct gameState *post, int handpos)
 	struct gameState pre;
 	memcpy(&pre, post, sizeof(struct gameState));
 	
-	//testing smithy fucntion
-	int r = smithy(player, post, handpos);
+	//testing smithy function
+	int r = test_smithyCard(player, post, handpos);
+	int drawnCardCount = pre.deckCount[player] - post->deckCount[player];
 	
-	//What smithy function is supposed to do.
-	int k;
-	for( k = 0; k < 3; k++)
+	if( post->deckCount[player] == (pre.deckCount[player]-3))
 	{
-		drawCard(player,&pre);
-	}
-	discardCard(handpos, player, &pre, 0)
-
-	//Check if the two gameStates are the same
-	if(pre.discardCount[player] == post->discardCount[p])
-	{
-		if(pre.handCount[player] == post->handCOunt[player])
+		if( post->handCount[player] == (pre.handCount[player]+2))
 		{
-			return 0;
+			if( post->playedCardCount == (pre.playedCardCount+1))
+			{	//test passed
+				printf("test PASSED\n");
+				return drawnCardCount;
+			}
+			else//play card count incorrect
+			{
+				printf("***** FAILED TEST ***** played card count incorrect\n");
+				return drawnCardCount;
+			}
+		}
+		else//hand count is incorrect
+		{
+			printf("*****FAILED TEST ***** hand count incorrect\n");
+			return drawnCardCount;
 		}
 	}
-	return -1;
-
-}
-
+	else//deck count is incorrect
+	{
+		printf("***** FAILED TEST ***** deck count incorrect\n");
+		return drawnCardCount;
+	}			
+};
 
 int main()
 {
 	//set up game
 	int max_hand = 500;
 	int max_deck = 500; 
-	int i, j, handIndex, p, randNum;
+	int i, j, handIndex, p, randNum, preDeck, preHand, postDeck, postHand;
 	int NUM_PLAYERS= 2;
 	int numCards = 17;
 	int seed = 9999;
@@ -54,9 +62,9 @@ int main()
 	int gameCards[17] = {adventurer, council_room, feast, gardens, mine, remodel, smithy, minion, baron, sea_hag, copper, silver, gold, estate, duchy, province, curse};
 
 
-	for ( i = 0; i < 20; i++)
+	for ( i = 0; i < 100; i++)
 	{
-		printf("\n\n************** test iteration %d **************\n", i+1);
+		printf("\n\n-------------------- Smithy Card Test Iteration %d --------------------\n", i+1);
 		initializeGame(NUM_PLAYERS, kingdomcards, seed, &G);
 		
 		//randomize player turn, deck, hand, and discard count
@@ -84,15 +92,25 @@ int main()
 		//inserting smithy card into random hand index
 		handIndex = rand() %(G.handCount[p]);
 		G.hand[p][handIndex] = smithy;
-		//print pre-test stats
-		printf(" - PRE -\n Player: %d\n deck count: %d	discard count: %d	hand count: %d\n smithy in hand index: %d\n", p, G.deckCount[p], G.discardCount[p], G.handCount[p], handIndex);
 		
+		//saving pre-test stats
+		preDeck = G.deckCount[p];
+		preHand = G.handCount[p];
+		
+		//print pre-test stats
+		printf(" ____PRE____\n Player: %d\n deck count: %d		discard count: %d	hand count: %d\n smithy in hand index: %d\n", p, G.deckCount[p], G.discardCount[p], G.handCount[p], handIndex);
+		
+		//run test
+		printf("\n			TEST RESULTS		\n");
 		int test = checkSmithy(p, &G, 0);
-		if (test != 0 )
-		{
-			printf("checkSmithy() FAILED\n");
-		}
-		else{printf("checkSmithy() PASSED\n");}
+
+		//calculate post-test stats
+		postDeck = preDeck - test;
+		postHand = preHand+2;
+		
+		// print post-test game stats
+		printf("\n ____POST____\n deck count: %d 	card drawn count: %d 	played card count: %d	hand count: %d\n", G.deckCount[p], test, G.playedCardCount, G.handCount[p]);
+		printf("\n\nExpected Results:\n deck count: %d 	cards drawn count: 3 	played card count: 1	hand count: %d\n", postDeck, postHand);
 	}
 	return 0;
 }
